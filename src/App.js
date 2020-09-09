93306ab4daa8008cac317e786a9299e8b0702a82
MikeMor-fr <62838338+MikeMor-fr@users.noreply.github.com>
2020-09-09 15:22:19 +0200

    Added Search Box filter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./styles.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -17,9 +19,20 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(event) =>
+            this.setState({ searchField: event.target.value })
+          }
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
